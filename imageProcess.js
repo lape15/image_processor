22e53bb3fce9c3f5d881d0787ca8e3218ffefe0d
Lape <larape1993@gmail.com>
2025-03-13 13:22:52 +0100

    More items

---
+++ imageProcess.js
@@ -0,0 +1,53 @@
+const { isMainThread, parentPort, workerData } = require("worker_threads");
+const AWS = require("aws-sdk");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const sizes = [
+  { suffix: "small", width: 200 },
+  { suffix: "medium", width: 500 },
+  { suffix: "large", width: 800 },
+];
+
+async function processImage() {
+  const { imgUrl, key } = workerData;
+  try {
+    const response = await fetch(imgUrl);
+    if (!response.ok)
+      throw new Error(`Failed to fetch image: ${response.statusText}`);
+    const imageBuffer = Buffer.from(await response.arrayBuffer());
+    for (const size of sizes) {
+      const resizedImageBuffer = await sharp(imageBuffer)
+        .resize(size.width)
+        .toBuffer();
+      const newKey = `processed/${size.suffix}/${path.basename(key)}`;
+      await s3
+        .upload({
+          Bucket: process.env.S3_BUCKET_NAME,
+          Key: newKey,
+          Body: resizedImageBuffer,
+          ContentType: "image/jpeg",
+        })
+        .promise();
+      parentPort.postMessage({
+        status: "success",
+        size: size.suffix,
+        key: newKey,
+      });
+      parentPort.close();
+    }
+  } catch (err) {
+    parentPort.postMessage({ status: "error", error: error.message });
+  }
+}
+
+if (isMainThread) {
+  processImage();
+}
---
+++ index.js
@@ -0,0 +1,69 @@
+const { S3Client } = require("@aws-sdk/client-s3");
+const express = require("express");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const path = require("path");
+const PORT = 5000;
+
+require("dotenv").config();
+
+app.use(express.static("public"));
+const app = express();
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION,
+  },
+});
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.S3_BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    metadata: function (req, file, cb) {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: function (req, file, cb) {
+      cb(null, `uploads/${Date.now()}-${file.originalname}`);
+    },
+  }),
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded");
+  }
+
+  const worker = new Worker("./imageProcess.js", {
+    workerData: {
+      imgUrl: req.file.location,
+      key: req.file.key,
+    },
+  });
+  worker.on("message", (message) => {
+    console.log(message);
+  });
+  worker.on("error", (error) => {
+    console.error("Worker Error:", error);
+  });
+
+  worker.on("exit", (code) => {
+    if (code !== 0) console.error(`Worker stopped with exit code ${code}`);
+  });
+  res.json({
+    message: "Upload successful, processing started",
+    file: req.file.location,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
